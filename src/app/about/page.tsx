4c7aa6faae0b93c8a934737b57a2f11f375fd4d0
mahdialsholi <mahdi.alsholi.1985@gmail.com>
2025-02-23 23:59:19 +0200

    Add image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import CloudImage from "../../../public/cloud-hosting.png";
 
 const AboutPage = () => {
   return (
-    <div className='fix-height'>
-      AboutPage
-    </div>
-  )
-}
+    <section className="fix-height container m-auto">
+      <h1 className="text-3xl font-bold text-gray-800 p-5 ">AboutPage</h1>
+      <div className="">
+        {/* <Image src="/cloud-hosting.png" width={400} height={400} alt="" /> */}
+        <Image
+          src={CloudImage}
+          alt=""
+          width={500}
+          height={500}
+          priority={true}
+        />
+      </div>
+    </section>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
